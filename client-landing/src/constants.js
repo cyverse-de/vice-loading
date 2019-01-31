8b3c2bf5af88b6d0699a0caafbedb6860eb3d409
John Wregglesworth <wregglej@iplantcollaborative.org>
2019-01-31 13:56:32 -0700

    Begin splitting the landing and loading pages into separate projects

--- client-landing/src/constants.js
+++
@@ -1,6 +0,0 @@
-export default ({
-    LOGIN_URL: "/auth/provider",
-    LOGOUT_URL: "/logout",
-    GRAVATAR_URL: "https://www.gravatar.com/avatar/",
-    GRAVATAR_OPTIONS: "d=identicon&s=60",
-});
--- server/src/apps.js
+++
@@ -1,9 +0,0 @@
-const fetch = require('node-fetch');
-const debug = require('debug')('permissions');
-
-
-export async function getAppsForUser(user) {
-    const appsURL = process.env.APPS + user;
-    debug(`fetching apps from ${appsURL}`);
-    return fetch(process.env.APPS + user);
-}
