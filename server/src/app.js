057d718e3f9cc5d147ab977fb23767b0a2a10c7c
John Wregglesworth <wregglej@cyverse.org>
2019-02-01 07:55:17 -0700

    Re-add db import to app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import db from './db';
 import hasValidSubdomain, { extractSubdomain } from './subdomain';
 import { endpointConfig, ingressExists } from './ingress';
 import compression from 'compression';
