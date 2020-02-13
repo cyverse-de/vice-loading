3a841e3c65a79bfc79860504f76db03546b498f9
John Wregglesworth <wregglej@iplantcollaborative.org>
2020-02-13 16:16:40 -0700

    Introduce a bit more sanity. Just a bit.

--- src/server/apiRouter.js
+++ src/server/apiRouter.js
@@ -1,40 +1,11 @@
 import express from "express";
-import { getDB } from "./db";
+import url from "url";
 import hasValidSubdomain, { extractSubdomain } from "./subdomain";
 import { endpointConfig, ingressExists } from "./ingress";
-import compression from "compression";
-import helmet from "helmet";
 import noCache from "nocache";
-import morgan from "morgan";
-import path from "path";
-import url from "url";
 
 const fetch = require("node-fetch");
 const debug = require("debug")("app");
-
-const db = getDB();
-
-const app = express();
-app.use(compression());
-app.use(helmet());
-app.use(morgan("combined"));
-
-app.use("/healthz", noCache());
-app.get(
-    "/healthz",
-    async (req, res) =>
-        await db
-            .one(
-                `select version from version order by applied desc limit 1;`,
-                []
-            )
-            .then((version) => res.status(200).send(version))
-            .catch((e) =>
-                res
-                    .status(500)
-                    .send(`error fetching version from database: ${e}`)
-            )
-);
 
 const apirouter = express.Router();
 
@@ -143,10 +114,4 @@ apirouter.get("/url-ready", async (req, res) => {
     res.send(JSON.stringify({ ready: ready }));
 });
 
-app.use("/api", apirouter);
-
-const uiDir = "../../client-loading/build";
-const uiPath = path.join(__dirname, uiDir);
-app.use(express.static(uiPath));
-
-export default app;
+export default apirouter;
--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,61 @@
-// Entrypoint into the server-side VICE UI code.
-//
-// Check the ../.env.example file to see what configuration settings need to be
-// set.
-import app from "./app";
-const port = process.env.PORT || 60000;
-app.listen(port, () => console.log(`example app listening on port ${port}!`));
+import express from "express";
+import next from "next";
+import { getDB } from "./db";
+import apirouter from "./apiRouter";
+
+import compression from "compression";
+import helmet from "helmet";
+import noCache from "nocache";
+import morgan from "morgan";
+
+const db = getDB();
+
+const app = next({
+    dev: true,
+});
+const nextHandler = app.getRequestHandler();
+
+app.prepare()
+    .then(() => {
+        const server = express();
+        server.use(compression);
+        server.use(helmet());
+        server.use(morgan("combines"));
+
+        server.use("/healthz", noCache());
+        server.get(
+            "/healthz",
+            async (req, res) =>
+                await db
+                    .one(
+                        `select version from version order by applied desc limit 1;`,
+                        []
+                    )
+                    .then((version) => res.status(200).send(version))
+                    .catch((e) =>
+                        res
+                            .status(500)
+                            .send(`error fetching version from database: ${e}`)
+                    )
+        );
+
+        server.use("/api", apirouter);
+
+        server.get("*", (req, res) => {
+            return nextHandler(req, res);
+        });
+
+        const port = process.env.PORT || 60000;
+
+        server.listen(port, (err) => {
+            if (err) throw err;
+            console.log(`> Ready on http://localhost:${port}`);
+        });
+    })
+
+    .catch((exception) => {
+        console.log(exception);
+        process.exit(1);
+    });
+
+export default app;
