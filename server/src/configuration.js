103b5b8c9be274414fc07359c33690433aafa167
John Wregglesworth <wregglej@cyverse.org>
2020-03-30 12:12:10 -0700

    Fix value of the k8s_enable configuration option

--- server/src/configuration.js
+++ server/src/configuration.js
@@ -4,5 +4,5 @@ export const listenPort = config.get("listen_port");
 export const viceDomain = config.get("vice_domain");
 export const db = config.get("db");
 export const ui = config.get("ui");
-export const k8sEnabled = config.get("k8s_enabled") === "true";
+export const k8sEnabled = config.get("k8s_enabled");
 export const appExposerURL = config.get("app_exposer.url");
